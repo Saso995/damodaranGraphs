5560f7bf5b6469e7d7a9c71fa35804ae1c7ed083
Salvatore Bova <salvatore_bova@hotmail.it>
2020-04-26 15:21:08 +0200

    up

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+const mongo = require("./db/connection");
+var mong = require('mongodb')
 
 
 app.use(bodyParser.json());
@@ -9,6 +11,19 @@ app.use(express.static(__dirname + "/public"));
 
 var PORT = process.env.PORT || 8080;
 
-app.listen(PORT, function(){
-  console.log("Listening at port 8080");
+mongo.connectToServer( function( err, client ) {
+  if (err) console.log(err);
+  app.listen(PORT, function(){
+    console.log("Listening at port 8080");
+  });
 });
+
+app.get('/graphsPage', function (req, res) {
+  var db = mongo.getDb();
+  pagName = req.query.id
+  var query = { pageId: pagName };
+  db.collection("pages").find(query).toArray(function(err, result) {
+    if (err) throw err;
+    res.send(result)
+  });
+})

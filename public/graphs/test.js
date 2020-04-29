9b137ba1884b9cca5eac2a3b4f95cc7621a1cc58
Salvatore Bova <salvatore_bova@hotmail.it>
2020-04-29 22:50:06 +0200

    up

---
+++ public/graphs/test.js
@@ -0,0 +1,19 @@
+  const capture = () => {
+    const body = document.querySelector('body');
+    body.id = 'capture';
+    html2canvas(document.querySelector("#capture")).then(canvas => {
+      document.body.appendChild(canvas);
+    }).then(() => {
+      var canvas = document.querySelector('canvas');
+      canvas.style.display = 'none';
+      var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+      var a = document.createElement("a");
+      a.setAttribute('download', 'pivotTable.png');
+      a.setAttribute('href', image);
+      a.click();
+    });
+  };
+
+
+  const btn = document.getElementById('btn');
+  btn.addEventListener('click', capture)
